e3f46a2792dea4dfd2b71f6345da05bdf3406d72
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-01-21 20:10:06 +0800

    Experimented REST methods

--- admin-console/admin-console/routes/api.php
+++ admin-console/admin-console/routes/api.php
@@ -7,9 +7,54 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/hello', function(){
+Route::get('/hello', function (Request $request) {
     return response()->json([
-        'message'=> 'Hello from Laravel!',
-        'value'=> 55
+        'message' => 'Hello from Laravel',
+        'value' => '55'
     ]);
 });
+
+/*
+Route::post('/hello', function (Request $request) {
+    return response()->json([
+        'message'=> 'You sent: ' . $request->input('text'),
+        'author' => 'The Author: ' . $request->input('author')
+        ]);
+        
+});
+
+//Other Post Method Example
+
+Route::post('/hello/create', function (Request $request) {
+    return response()->json([
+        'id' => rand(1, 100000),
+        'message'=> 'You sent: ' . $request->input('text'),
+        'author' => 'The Author: ' . $request->input('author')
+        ]);
+        
+});
+
+Route::put('/hello/{id}', function ($id, Request $request) {
+    return response()->json([
+        'id'=> $id,
+        'updated' => $request->all()
+        ]);
+        
+});
+
+Route::patch('/hello/{id}', function ($id, Request $request) {
+    return response()->json([
+        'id'=> $id,
+        'patched' => $request->all()
+        ]);
+        
+});
+
+Route::delete('/hello/{id}', function ($id, Request $request) {
+    return response()->json([
+        'message' => "Deleted resource with id of $id"
+        ]);
+        
+});
+
+*/
