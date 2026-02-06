32d5e71766191b69951270a5a71dfaa21d185b46
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-02-06 22:50:50 +0800

    Defined high-importance channel

--- mobile-client/monitus_mobile_client/lib/main.dart
+++ mobile-client/monitus_mobile_client/lib/main.dart
@@ -4,6 +4,7 @@ import 'providers/registration_provider.dart';
 import 'screens/registration_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
 void main() async{
@@ -18,6 +19,23 @@ void main() async{
   } catch (e){
     debugPrint("Firebase initialisation failed! $e");
   }
+
+  // Define High-Importance Channel
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'Emergency Alerts',
+    description: 'This channel is used for critical emergency notifications.',
+    importance: Importance.max,
+    playSound: true
+  );
+
+  // Create the channel on the device
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationPlugin =
+    FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
   
 
   runApp(
