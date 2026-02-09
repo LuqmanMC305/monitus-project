9ca9f0e87f5b67ef9e3c8fe608dbdd624f3d7f76
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-02-09 21:20:38 +0800

    Added the Incident Map at navigation bar

--- admin-console/admin-console/resources/views/navigation-menu.blade.php
+++ admin-console/admin-console/resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('incident.map') }}" :active="request()->routeIs('incident-map')">
+                        {{ __('Incident Map') }}
+                    </x-nav-link>
                 </div>
             </div>
 
