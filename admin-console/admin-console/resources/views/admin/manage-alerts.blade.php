7e5a6c35b543c589fcfbbce54f642ae31dd7ae15
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-05-07 23:11:31 +0800

    Removed small typo

--- admin-console/admin-console/resources/views/admin/manage-alerts.blade.php
+++ admin-console/admin-console/resources/views/admin/manage-alerts.blade.php
@@ -84,7 +84,7 @@
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    <h3 class="text-lg font-semibold uppercase tracking-wider">Resolved Incident History></h3>
+                    <h3 class="text-lg font-semibold uppercase tracking-wider">Resolved Incident History</h3>
                 </div>
 
                 <div class="overflow-x-auto">
