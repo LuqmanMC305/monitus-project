e2952cbf987856c77b534fd2eccc864d416ff65a
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-06-01 19:52:21 +0800

    Enable the local url support herd expose

--- mobile-client/monitus_mobile_client/lib/config/api_config.dart
+++ mobile-client/monitus_mobile_client/lib/config/api_config.dart
@@ -6,7 +6,7 @@ class ApiConfig{
   static String get baseUrl {
     switch (current){
       case Environment.local:
-        return 'http://192.168.0.195/api';
+        return 'https://3w74rbh0xh.sharedwithexpose.com/api';
       
       case Environment.staging:
         return 'https://monitus-laravel-backend-49hltibe.on-forge.com/api';
